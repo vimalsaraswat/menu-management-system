7820fa2348e0ddb72fdb1fdb6fce6a2663889fca
Vimal Saraswat <saraswatvimal1@gmail.com>
2025-11-20 01:46:32 +0530

    Redirect logout to /auth/login

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -32,7 +32,7 @@ export default async function DashboardLayout({
               action={async () => {
                 "use server";
                 await removeAuthCookie();
-                redirect("/login");
+                redirect("/auth/login");
               }}
             >
               <Button variant="ghost" size="sm">
